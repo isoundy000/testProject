9c99d5bea897e4fe43c476c421fd2c0ea2c6f3a2
kinglong <1406733029@qq.com>
2017-11-28 17:32:31 +0800

    项目目录整理

---
+++ src/resource.js
@@ -0,0 +1,27 @@
+var res = {
+    dlgNnFpScene_json : "res/game_nnfp/dlgNnFpScene.json",
+    nnFpAnimationPlist_plist: "res/game_nnfp/nnFpAnimationPlist.plist",
+    dlgNnFpPlayer_json: "res/game_nnfp/dlgNnFpPlayer.json",
+    huaiFengCardPlist_plist: "res/public/huaiFengCard/huaiFengCardPlist.plist",
+    huaiFengCardPlist_png: "res/public/huaiFengCard/huaiFengCardPlist.png",
+    dlgNnFpClock_json: "res/game_nnfp/dlgNnFpClock.json",
+    dlgNnFpOpen_json: "res/game_nnfp/dlgNnFpOpen.json",
+    dlgNnFpReady_json: "res/game_nnfp/dlgNnFpReady.json",
+    dlgNnFpSystem_json: "res/game_nnfp/dlgNnFpSystem.json",
+    dlgNnFpGetType_json: "res/game_nnfp/dlgNnFpGetType.json",
+    dlgNnFpChooseTextChatScene_json: "res/game_nnfp/dlgChooseTextChatScene.json",
+    tx1huoyan_plist: "res/game_nnfp/tx1huoyan.plist",
+    nnFpWinOrLose_json : "res/game_nnfp/nnFpWinOrLoseNode.json",
+    nnFpSet_json : "res/game_nnfp/dlgNnFpSet.json",
+    nnFpRule_json : "res/game_nnfp/dlgNnFpRule.json",
+    nnFpResultScene_json : "res/game_nnfp/dlgNnFpResultScene.json",
+    nnFpReminder_json : "res/game_nnfp/dlgReminder.json",
+    nnFpRobBanker_json : "res/game_nnfp/dlgNnFpRobBanker.json",
+    nnFpAddChip_json : "res/game_nnfp/dlgNnFpAddChip.json",
+    nnFpGamePlist_plist: "res/game_nnfp/gameNnFpPlist.plist"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
